8f43a13c3b6a2ffda5a6c1d3d4d3e3674de4ef5c
Huiming Jia <jiahm92@gmail.com>
2016-10-18 18:36:37 -0400

    Update LemmaIndexMapred.java

--- InvertedIndex/src/main/java/code/lemma/LemmaIndexMapred.java
+++ InvertedIndex/src/main/java/code/lemma/LemmaIndexMapred.java
@@ -1,13 +1,16 @@
 package code.lemma;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Mapper.Context;
 
-import util.StringIntegerList;
 import edu.umd.cloud9.collection.wikipedia.WikipediaPage;
+import mapreduce.util.StringIntegerList;
 
 /**
  * 
@@ -20,6 +23,26 @@ public class LemmaIndexMapred {
 		public void map(LongWritable offset, WikipediaPage page, Context context) throws IOException,
 				InterruptedException {
 			// TODO: implement Lemma Index mapper here
+			
+//			Tokenizer tokenier=new Tokenizer();
+			Text title=new Text();
+			title.set(page.getTitle());
+//			List<String> content=tokenier.tokenize(page.getContent());	
+			String[] content=page.getContent().split(" ");
+			HashMap<String,Integer> word_frequency=new HashMap<String,Integer>();
+			for(String lemma:content){
+				if(word_frequency.containsKey(lemma)){
+					Integer temp=new Integer(word_frequency.get(lemma).intValue()+1);
+					word_frequency.remove(lemma);
+					word_frequency.put(lemma,temp);
+				}
+				else{
+					word_frequency.put(lemma, new Integer(1));
+				}			
+			}			
+			StringIntegerList stringIntegerList=new StringIntegerList(word_frequency);		
+			context.write(title,stringIntegerList);				
 		}
 	}
 }
+
